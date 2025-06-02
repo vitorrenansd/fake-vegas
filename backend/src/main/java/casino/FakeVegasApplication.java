6eaebd9fb8d6d5a91a81a2a895e8bd79e3f69471
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-02 14:32:54 -0300

    Initial commit

---
+++ backend/src/main/java/casino/FakeVegasApplication.java
@@ -0,0 +1,13 @@
+package casino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeVegasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeVegasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/casino/FakeVegasApplicationTests.java
@@ -0,0 +1,13 @@
+package casino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeVegasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
