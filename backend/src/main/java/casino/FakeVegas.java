0c0796e140ce391e2cc2883929715f96598cd1ac
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-02 14:45:33 -0300

    New main class name

--- backend/src/main/java/casino/FakeVegas.java
+++ backend/src/main/java/casino/FakeVegas.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FakeVegasApplication {
-
+public class FakeVegas {
 	public static void main(String[] args) {
-		SpringApplication.run(FakeVegasApplication.class, args);
+		SpringApplication.run(FakeVegas.class, args);
 	}
-
 }
