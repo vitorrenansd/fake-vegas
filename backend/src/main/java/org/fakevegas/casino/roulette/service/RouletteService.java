14af9951cd0088cf2ec53c134179572546eafb9b
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-03 17:30:42 -0300

    Created RouletteService class with play()

--- backend/src/main/java/org/fakevegas/casino/roulette/service/RouletteService.java
+++ backend/src/main/java/org/fakevegas/casino/roulette/service/RouletteService.java
@@ -2,9 +2,20 @@ package org.fakevegas.casino.roulette.service;
 
 import org.fakevegas.casino.roulette.dto.RouletteRequest;
 import org.fakevegas.casino.roulette.dto.RouletteResult;
+import org.fakevegas.casino.roulette.model.Colors;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 @Service
 public class RouletteService {
+    private final Random random = new Random();
 
+    public RouletteResult play(RouletteRequest request) {
+        Colors[] colors = Colors.values();
+        Colors rolled = colors[random.nextInt(colors.length)]; // pseudorandom, i need to fix this
+
+        boolean win = rolled == request.color();
+        return new RouletteResult(rolled, win);
+    }
 }
