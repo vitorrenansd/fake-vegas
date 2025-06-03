19602bb8d8edb16df33a87ae9bf50c906b7ee0d5
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-03 18:34:31 -0300

    Now using Slots enum in win condition

--- backend/src/main/java/org/fakevegas/casino/roulette/service/RouletteService.java
+++ backend/src/main/java/org/fakevegas/casino/roulette/service/RouletteService.java
@@ -2,7 +2,7 @@ package org.fakevegas.casino.roulette.service;
 
 import org.fakevegas.casino.roulette.dto.RouletteRequest;
 import org.fakevegas.casino.roulette.dto.RouletteResult;
-import org.fakevegas.casino.roulette.model.Colors;
+import org.fakevegas.casino.roulette.model.Slots;
 import org.springframework.stereotype.Service;
 
 import java.util.Random;
@@ -12,10 +12,10 @@ public class RouletteService {
     private final Random random = new Random();
 
     public RouletteResult play(RouletteRequest request) {
-        Colors[] colors = Colors.values();
-        Colors rolled = colors[random.nextInt(colors.length)]; // pseudorandom, i need to fix this
+        Slots[] slots = Slots.values();
+        Slots rolledSlot = slots[random.nextInt(slots.length)]; // pseudorandom, i need to fix this
 
-        boolean win = rolled == request.color();
-        return new RouletteResult(rolled, win);
+        boolean win = rolledSlot.getColor() == request.color();
+        return new RouletteResult(rolledSlot.getColor(), win);
     }
 }
