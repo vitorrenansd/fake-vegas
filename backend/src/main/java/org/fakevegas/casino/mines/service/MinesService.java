9889612cc683cd36e587896ad29a2b621e5c24ab
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-04 15:38:34 -0300

    Added service methods from model.MinesGame

--- backend/src/main/java/org/fakevegas/casino/mines/service/MinesService.java
+++ backend/src/main/java/org/fakevegas/casino/mines/service/MinesService.java
@@ -1,18 +1,69 @@
 package org.fakevegas.casino.mines.service;
 
-import org.fakevegas.casino.mines.model.MinesGame;
+import org.fakevegas.casino.mines.dto.ClickRequest;
+import org.fakevegas.casino.mines.dto.MinesRequest;
+import org.fakevegas.casino.mines.dto.MinesResult;
 import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class MinesService {
-    private MinesGame currentGame;
+    private List<Integer> field;
+    private final int fieldSize = 25;
+    private int betAmount;
+    private int mines;
+    private boolean exploded;
 
-    public MinesGame startGame(int betAmount, int mines) {
-        currentGame = new MinesGame(betAmount, mines);
-        return currentGame;
+    public MinesResult startGame(MinesRequest request) {
+        this.betAmount = request.getBetAmount();
+        this.mines = request.getMines();
+        this.exploded = false;
+        this.field = generateField(mines);
+        return toResult();
     }
 
-    public MinesGame getCurrentGame() {
-        return this.currentGame;
+    public int getBetAmount() {
+        return this.betAmount;
+    }
+
+    public MinesResult click(ClickRequest clickRequest) {
+        if (exploded || field == null) return toResult();
+
+        int index = clickRequest.getIndex();
+        if (index < 0 || index >= 25) return toResult();
+
+        int value = field.get(index);
+        if (value == 1) {
+            exploded = true;
+        } else if (value == 0) {
+            field.set(index, 2); // marcar como clicado
+        }
+        return toResult();
+    }
+
+    private List<Integer> generateField(int bombs) {
+        List<Integer> newField = new ArrayList<>();
+        for (int i = 0; i < bombs; i++) newField.add(1); // bombas
+        for (int i = bombs; i < fieldSize; i++) newField.add(0); // seguros
+        Collections.shuffle(newField);
+        return newField;
+    }
+
+    private double calculateMultiplier() {
+        int totalSafe = fieldSize - mines;
+        long safeRevealed = field.stream().filter(i -> i == 2).count();
+
+        double multiplier = 1.0;
+        for (int i = 0; i < safeRevealed; i++) {
+            multiplier *= (double)(totalSafe - i) / (fieldSize - i);
+        }
+        double rtp = 0.99;
+        return Math.round((1.0 / multiplier) * rtp * 100.0) / 100.0;
+    }
+
+    private MinesResult toResult() {
+        return new MinesResult(exploded, calculateMultiplier(), field);
     }
 }
