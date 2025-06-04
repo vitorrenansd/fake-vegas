a9e8322493e8236b7f2af0938bb1ee1ee468dc78
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-04 15:37:21 -0300

    Removed model methods, now runs inside Service class (better use)

--- backend/src/main/java/org/fakevegas/casino/mines/model/MinesGame.java
+++ backend/src/main/java/org/fakevegas/casino/mines/model/MinesGame.java
@@ -1,60 +1,36 @@
 package org.fakevegas.casino.mines.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class MinesGame {
-    private final int bet;
-    private final int mines;
-    private final int fieldSize = 25;
     private List<Integer> field;
-    private boolean exploded = false;
+    private int betAmount;
+    private int mines;
+    private boolean exploded;
 
-    public MinesGame(int bet, int mines) {
-        this.bet = bet;
+    public MinesGame(List<Integer> field, int betAmount, int mines) {
+        this.field = field;
+        this.betAmount = betAmount;
         this.mines = mines;
-        this.field = createField(mines);
+        this.exploded = false;
     }
 
-    private List<Integer> createField(int bombs) {
-        List<Integer> field = new ArrayList<>();
-        for (int i = 0; i < bombs; i++) { 
-            field.add(1); // Fill bombs
-        }
-        for (int i = bombs; i < fieldSize; i++) {
-            field.add(0); // Fill remaining with empty
-        }
-
-        Collections.shuffle(field);
+    public List<Integer> getField() {
         return field;
     }
-
-    public boolean click(int index) {
-        if (exploded || index < 0 || index >= fieldSize) return false;
-
-        int clickedIdx = field.get(index);
-
-        if (clickedIdx == 1) { // If has a bomb
-            exploded = true;
-            return false;
-        } else if (clickedIdx == 0) { // If not
-            field.set(index, 2);
-            return true;
-        }
-        return true; // Already clicked
-    }
-
-    public int getBet() {
-        return this.bet;
+    public int getBetAmount() {
+        return betAmount;
     }
     public int getMines() {
-        return this.mines;
+        return mines;
     }
     public boolean isExploded() {
         return exploded;
     }
-    public List<Integer> getField() {
-        return field;
+    public void setField(List<Integer> field) {
+        this.field = field;
+    }
+    public void setExploded(boolean exploded) {
+        this.exploded = exploded;
     }
 }
