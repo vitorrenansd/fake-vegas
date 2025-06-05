445b6a5155a74d40ac56ab97eb14d311dfa30a27
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-05 14:51:50 -0300

    Created first version of main menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="title-container">
+        <h1>FAKE VEGAS</h1>
+        <p className="subtitle">Your Ultimate Casino Experience</p>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="button-container">
+          <button className="game-button">
+            <img src="/images/dice-icon.svg" alt="Dice" className="button-icon" />
+            <span className="button-text">DICE</span>
+          </button>
+          <button className="game-button">
+            <img src="/images/bomb-icon.svg" alt="Mines" className="button-icon" />
+            <span className="button-text">MINES</span>
+          </button>
+          <button className="game-button">
+            <img src="/images/roulette-icon.svg" alt="Roulette" className="button-icon" />
+            <span className="button-text">ROULETTE</span>
+          </button>
+        </div>
       </header>
     </div>
   );
