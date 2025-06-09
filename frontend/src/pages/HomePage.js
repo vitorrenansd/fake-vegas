d0a75f058306aad75fd3e7c9a7bb73a84df39798
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-09 13:26:05 -0300

    Now HomePage  buttons implements useNavigate()

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,6 +1,9 @@
 import './HomePage.css';
+import { useNavigate } from 'react-router-dom';
 
 export const HomePage = () => {
+    const navigate = useNavigate();
+
     return (
     <div className="App">
         <div className="title-container">
@@ -9,15 +12,15 @@ export const HomePage = () => {
         </div>
         <header className="App-header">
           <div className="button-container">
-            <button className="game-button">
+            <button className="game-button" onClick={() => navigate('/dice')}>
               <img src="/images/dice-icon.svg" alt="Dice" className="button-icon" />
               <span className="button-text">DICE</span>
             </button>
-            <button className="game-button">
+            <button className="game-button" onClick={() => navigate('/mines')}>
               <img src="/images/bomb-icon.svg" alt="Mines" className="button-icon" />
               <span className="button-text">MINES</span>
             </button>
-            <button className="game-button">
+            <button className="game-button" onClick={() => navigate('/roulette')}>
               <img src="/images/roulette-icon.svg" alt="Roulette" className="button-icon" />
               <span className="button-text">ROULETTE</span>
             </button>
