8b4bf7a1bda3ee36d7084176f17e951521055f27
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-06-09 13:08:33 -0300

    Now using react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { DiceGamePage } from './pages/DiceGamePage';
+import { MinesGamePage } from './pages/MinesGamePage';
+import { RouletteGamePage } from './pages/RouletteGamePage';
+
 function App() {
   return (
-    <h1>oi</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dice" element={<DiceGamePage />} />
+        <Route path="/mines" element={<MinesGamePage />} />
+        <Route path="/roulette" element={<RouletteGamePage />} />
+      </Routes>
+    </Router>
   );
 }
 
